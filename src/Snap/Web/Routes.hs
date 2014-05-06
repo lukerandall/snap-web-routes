637172f2fd2302dd6026ffdaa74269faeb77f5d8
Luke Randall <luke.randall@gmail.com>
2014-05-06 09:40:05 +0200

    Fix up haddocks

--- src/Snap/Web/Routes.hs
+++ src/Snap/Web/Routes.hs
@@ -1,92 +1,94 @@
-{-|
-
-This module provides a ready to use implementation of `web-routes` for Snap.
-
-To get going, you'll need to add a few things to `Application.hs`.
-
-> {-# LANGUAGE DeriveGeneric     #-}
-> {-# LANGUAGE FlexibleInstances #-}
-> {-# LANGUAGE TypeFamilies      #-}
-
-DeriveGeneric is used to derive the `PathInfo` instance for your URL data type,
-the rest are needed by `web-routes`.
-
-> import Data.Text (Text)
-> import Snap.Web.Routes
-
-"Snap.Web.Routes" exports the data types needed to define your `PathInfo` and
-`MonadRoute` instances below.
-
-> data AppUrl
->     = Count Int
->     | Echo Text
->     | Paths [Text]
->       deriving (Generic)
-
-Define your application's URL data type. Deriving a `Generic` instance gives
-you a `PathInfo` instance for free.
-
-> data App = App
->     { _routeFn :: AppUrl -> [(Text, Maybe Text)] -> Text
->     }
-
-Extend your App type to include a routing function.
-
-> instance PathInfo AppUrl
-
-Get your free PathInfo instance. Alternatives are to use `web-routes-th` or
-implement PathInfo yourself.
-
-> instance MonadRoute (Handler App App) where
->    type URL (Handler App App) = AppUrl
->    askRouteFn = gets _routeFn
-
-Define your MonadRoute instance. In particular, `type URL (Handler App App)`
-must be set to your URL data type defined above and `askRouteFn` should point
-to the routing function you added to your App type.
-
-Moving on to `Site.hs`.
-
-> import Snap.Web.Routes
-
-Snap.Web.Routes provides a convenience router function you'll need.
-
-> routes :: [(ByteString, Handler App App ())]
-> routes = [ ("",          serveDirectory "static")
->          , ("",          routeWith routeAppUrl)
->          ]
-
-Add your routes to the bottom of the routes list using routeWith.
-
-> routeAppUrl :: AppUrl -> Handler App App ()
-> routeAppUrl appUrl =
->     case appUrl of
->       (Count n)   -> writeText $ ("Count = " `T.append` (T.pack $ show n))
->       (Echo text) -> echo text
->       (Paths ps)  -> writeText $ T.intercalate " " ps
-
-> echo :: T.Text -> Handler App App ()
-> echo msg = heistLocal (bindString "message" msg) $ render "echo"
-
-Define the handler for each data constructor in your URL data type.
-
-> app :: SnapletInit App App
-> app = makeSnaplet "app" "An example application with snap-web-routes." Nothing $ do
->     addRoutes routes
->     return $ App renderRoute
-
-Lastly, add the routing function to your app. If you prefixed the routes in routeWith:
-
->          , ("/prefix",          routeWith routeAppUrl)
-
-then use `renderRouteWithPrefix` instead:
-
->     return . App $ renderRouteWithPrefix "/prefix"
-
-|-}
-
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies      #-}
+
+-- |
+-- This module provides a ready to use implementation of `web-routes` for Snap.
+--
+-- The tutorial assumes you have a standard Snap app with Application.hs and
+-- Site.hs.
+--
+-- To get going, you'll need to enable a few extensions in Application.hs:
+--
+-- @
+-- \-\- we'll need to derive a generic instance for our URL data type
+-- \{\-\# LANGUAGE DeriveGeneric     \#\-\}
+--
+-- \-\- needed by web-routes
+-- \{\-\# LANGUAGE FlexibleInstances \#\-\}
+-- \{\-\# LANGUAGE TypeFamilies      \#\-\}
+--
+-- \-\- Paths and params are of type Text.
+-- import Data.Text (Text)
+-- \-\- Snap.Web.Routes exports everything you need to
+-- \-\- define your PathInfo and MonadRoute instances.
+-- import Snap.Web.Routes
+--
+-- \-\- Your URL data type.  Deriving a `Generic` instance gives
+-- \-\- you a `PathInfo` instance for free.
+-- data AppUrl
+--     = Count Int
+--     | Echo Text
+--     | Paths [Text]
+--       deriving (Generic)
+--
+-- \-\- Extend your App type to include a routing function.
+-- data App = App
+--     { _routeFn :: AppUrl -> [(Text, Maybe Text)] -> Text
+--     }
+--
+-- \-\- Thanks to the wonders of Generic, an empty instance
+-- \-\- definition is all we need. Alternately, you can implement
+-- \-\- toPathSegments and fromPathSegments yourself.
+-- instance PathInfo AppUrl
+--
+-- \-\- Set URL (Handler App App) to your URL data type defined above
+-- \-\- and askRouteFn must point to the routing function you added to
+-- \-\- your App.
+-- instance MonadRoute (Handler App App) where
+--    type URL (Handler App App) = AppUrl
+--    askRouteFn = gets _routeFn
+-- @
+--
+-- Moving on to `Site.hs`.
+--
+-- @
+-- \-\- Snap.Web.Routes provides routing functions
+-- import Snap.Web.Routes
+--
+-- \-\- Add your new routes using routeWith
+-- routes :: [(ByteString, Handler App App ())]
+-- routes = [ ("", routeWith routeAppUrl)
+--          , ("", serveDirectory "static")
+--          ]
+--
+-- \-\- Define handlers for each data constructor in your URL data type.
+-- routeAppUrl :: AppUrl -> Handler App App ()
+-- routeAppUrl appUrl =
+--     case appUrl of
+--       (Count n)   -> writeText $ ("Count = " `T.append` (T.pack $ show n))
+--       (Echo text) -> echo text
+--       (Paths ps)  -> writeText $ T.intercalate " " ps
+--
+-- \-\- You'll note that these are normal Snap handlers, except they can take
+-- \-\- values from the data constructor as arguments. This is a lot nicer than
+-- \-\- having using getParam.
+-- echo :: T.Text -> Handler App App ()
+-- echo msg = heistLocal (bindString "message" msg) $ render "echo"
+--
+-- \-\- Add the routing function to your app.
+-- app :: SnapletInit App App
+-- app = makeSnaplet "app" "An example snap-web-routes app." Nothing $ do
+--     addRoutes routes
+--     return $ App renderRoute
+-- @
+--
+-- If you prefixed the routes in routeWith
+--
+-- > ("/prefix", routeWith routeAppUrl)
+--
+-- then use `renderRouteWithPrefix` instead:
+--
+-- >     return . App $ renderRouteWithPrefix "/prefix"
 
 module Snap.Web.Routes
   ( renderRoute
